c453b6bd49da9cdc69e12de49163c11ce5727375
DavidSiegl <david.siegl95@gmail.com>
2023-10-08 15:32:58 +0200

    finished generator

--- Config-Generator/config_generator.py
+++ Config-Generator/config_generator.py
@@ -5,15 +5,17 @@ folder_path = input("Folder: ")
 files_array = []
 
 for file in os.listdir(folder_path):
-    files_array.append(file) 
-    if file == "config.xml":
-        with open(folder_path + "/config.xml", "r+") as f:
-            for player_id in files_array:
-                lines = f.readlines()
-                lines_count = len(lines)
-                lines.insert(lines_count - 2, "\t<record from='" + player_id + "to='graphics/pictures/person/" + player_id + "/portrait'/>\n".expandtabs(2))
-            f.truncate()
-            f.writelines(lines)
-            f.close()
-    #else:
-        
+    if file != "config.xml":
+        files_array.append(file.replace(".png", ""))
+    
+with open("./config_template.xml", "r") as f:
+    lines = f.readlines()
+    f.close()
+            
+lines_count = len(lines)
+for player_id in files_array:
+    lines.insert(lines_count - 2, "\t\t<record from=\"" + player_id + "\" to=\"graphics/pictures/person/" + player_id + "/portrait\"/>\n")
+                    
+with open(folder_path + "/config.xml", "w") as f:
+    f.writelines(lines)
+    f.close()      
